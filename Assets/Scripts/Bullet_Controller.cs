c7451c7b4a4b59a0d0861d9f826817bb4da57337
Joshua Randall <josh.randall16@gmail.com>
2017-08-26 01:26:47 -0400

    Importing Project
    -Startup files
    -Character controller
    -level
    -Folders for organizations

---
+++ Assets/Scripts/Bullet_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour {
+
+	public float Speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//moving the bullet in a vector 3 forward position
+		transform.Translate (Vector3.forward * Speed * Time.deltaTime);
+
+
+	}
+}
