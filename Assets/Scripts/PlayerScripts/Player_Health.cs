e53a842f6b5e506533fdaf5026255c964336d514
Joshua Randall <josh.randall16@gmail.com>
2017-08-26 19:25:05 -0400

    HealthPickup
    -Created health pick up object
    -Downloaded Horror music assets from Unity store

--- Assets/Scripts/PlayerScripts/Player_Health.cs
+++ Assets/Scripts/PlayerScripts/Player_Health.cs
@@ -9,6 +9,7 @@ public class Player_Health : MonoBehaviour {
 	public float maxHealth;
 	public float currentHealth;
 	private float dmg = 5.0f;	
+	private float healthUp = 5.0f;
 
 	public Slider healthBarSlider; // reference to slider on player
 
@@ -32,7 +33,7 @@ public class Player_Health : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-
+		//Colliding with Spider
 		if (other.gameObject.tag == "Spider") 
 		{
 			if (currentHealth <= maxHealth) 
@@ -50,7 +51,24 @@ public class Player_Health : MonoBehaviour {
 				//
 
 			}
+		
 		}
+	
+		//Colliding with HealthPack pickup
+		if(other.gameObject.tag == "HealthPack")
+		{
+
+			Debug.Log ("CRASHED INTO HEALTH PICKUP");
+
+			if (currentHealth < maxHealth) 
+			{
+				currentHealth += healthUp;
+				healthBarSlider.value += 0.05f;
+
+			}
+		}
+
+
 	}
 
 }
